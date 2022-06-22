a8f7624c60be01b01618f50435043ae907432358
Soliu Adeosun <sawlew@gmail.com>
2022-06-22 17:21:40 +0100

    first commit

---
+++ intro.js
@@ -0,0 +1,9 @@
+let nam = "Soliu Adeosun";
+let height = "5'7\"";
+let country = "Nigeria";
+
+
+document.write("<h1>Introduction to JavaScript</h1>");
+document.write("Name: " + nam + "<br>");
+document.write("Height: " + height + "<br>");
+document.write("Country: " + country);
